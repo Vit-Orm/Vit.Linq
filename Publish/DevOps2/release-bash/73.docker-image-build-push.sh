ad8940c082d3407df4efcf01a236b0ab63ea971c
lith <LithWang@yeah.net>
2024-01-17 22:14:56 +0800

    dev

--- Publish/DevOps2/release-bash/73.docker-image-build-push.sh
+++ Publish/DevOps2/release-bash/73.docker-image-build-push.sh
@@ -26,11 +26,11 @@ echo "73.docker-image-build-push.sh"
 
 if [ -d "$basePath/Publish/release/release/docker-image" ]; then
 
-	if [ "$DOCKER_Buildx" != "false" ]; then
-	    sh 75.docker-image-build-push_cross.bash
-	else
-	    sh 74.docker-image-build-push_amd64.bash
-	fi
+    if [ "$DOCKER_Buildx" != "false" ]; then
+        sh 75.docker-image-build-push_cross.bash
+    else
+        sh 74.docker-image-build-push_amd64.bash
+    fi
 else
     echo '73.docker-image-build-push.sh -> skip for no docker image files exist'
 fi
