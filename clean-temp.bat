403ba620363a3b99a7e29d98e1fdabfcb680af21
Lith <LithWang@outlook.com>
2024-06-02 23:24:48 +0800

    DevOps3

--- clean-temp.bat
+++ clean-temp.bat
@@ -1,5 +1,5 @@
-cd Publish\DevOps2\build-cmd
-VsTool.exe delete --path "..\..\..\src" --file "*.suo|*.user" --directory "obj|bin|.vs|packages"
+cd Publish\DevOps3\build-cmd
+VsTool.exe delete --path "..\..\.." --file "*.suo|*.user" --directory "obj|bin|.vs|packages"
 
 
 rd /s/q ..\..\release
