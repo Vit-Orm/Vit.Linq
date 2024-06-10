1fc666ac0eb71bb76bcdab87d10d528e233deb34
Lith <LithWang@outlook.com>
2024-06-11 01:38:47 +0800

    streamReader

--- src/Vit.Linq/ExpressionTree/CollectionsQuery/StreamReader.cs
+++ src/Vit.Linq/ExpressionTree/CollectionsQuery/StreamReader.cs
@@ -31,9 +31,8 @@ namespace Vit.Linq.ExpressionTree.CollectionsQuery
     }
 
 
-   
 
-    class Argument
+    public class Argument
     {
         Dictionary<string, ExpressionNode> parameterMap { get; set; }
 
@@ -103,72 +102,11 @@ namespace Vit.Linq.ExpressionTree.CollectionsQuery
 
         public ExpressionNode DeepClone(ExpressionNode node)
         {
-            var arg = this;
+            Func<ExpressionNode_Member, ExpressionNode> GetParameter = this.GetParameter;
 
-            var cloner = new ExpressionNodeCloner();
-            cloner.clone = (node) =>
-            {
-                if (node?.nodeType == NodeType.Member)
-                {
-                    ExpressionNode_Member member = node;
-
-                    ExpressionNode valueNode = null;
-
-                    if (!string.IsNullOrWhiteSpace(member.parameterName))
-                    {
-                        // {"nodeType":"Member", "parameterName":"a0", "memberName":"id"}
-                        valueNode = arg.GetParameter(member);
-                    }
-                    else if (member.objectValue?.nodeType == NodeType.Member)
-                    {
-                        // reduce level:  {"nodeType":"Member","objectValue":{"parameterName":"a0","nodeType":"Member"},"memberName":"id"}
-                        var objectNode = cloner.Clone(member.objectValue);
-                        valueNode = GetMember(objectNode, member.memberName, sourceMember: member);
-                    }
-
-                    valueNode = Reduce(valueNode);
-
-                    if (valueNode != null) return (true, valueNode);
-                }
-                return default;
-            };
-
-            return cloner.Clone(node);
-
-            ExpressionNode Reduce(ExpressionNode node)
-            {
-                if (node?.nodeType == NodeType.Member && node.objectValue != null)
-                {
-                    if (node.memberName == null)
-                    {
-                        return Reduce(node.objectValue);
-                    }
-                    else
-                    {
-                        if (node.objectValue.nodeType == NodeType.New)
-                        {
-                            return Reduce(GetMemberFromNewNode(node.objectValue, node.memberName));
-                        }
-                    }
-                }
-                return node;
-            }
-            ExpressionNode GetMember(ExpressionNode node, string memberName, ExpressionNode_Member sourceMember)
-            {
-                if (node == null || string.IsNullOrEmpty(memberName)) return node;
-
-                if (node.nodeType == NodeType.New)
-                {
-                    return GetMemberFromNewNode(node, memberName);
-                }
-                return ExpressionNode.Member(objectValue: node, memberName: memberName).Member_SetType(sourceMember.Member_GetType());
-            }
-
-            ExpressionNode GetMemberFromNewNode(ExpressionNode_New newNode, string memberName)
-            {
-                return newNode.constructorArgs?.FirstOrDefault(bind => bind.name == memberName)?.value ?? newNode.memberArgs?.FirstOrDefault(bind => bind.name == memberName)?.value;
-            }
+            return StreamReader.DeepClone(node, GetParameter);
         }
+   
     }
 
     public partial class StreamReader
@@ -531,8 +469,7 @@ namespace Vit.Linq.ExpressionTree.CollectionsQuery
 
             ExpressionNode sortField = resultSelector.body;
 
-            if (sortField?.nodeType != NodeType.Member)
-                throw new NotSupportedException($"[StreamReader] unexpected expression nodeType : {sortField.nodeType}");
+            //if (sortField?.nodeType != NodeType.Member) throw new NotSupportedException($"[StreamReader] unexpected expression nodeType : {sortField.nodeType}");
 
             var member = arg.DeepClone(sortField);
             return member;
