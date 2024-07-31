b8ac48f4a7ff30b36a4381bb589a815f631a53f0
Lith <LithWang@outlook.com>
2024-07-31 22:14:45 +0800

    change type of orders to List from IEnumerable add extra to PagedQuery and  RangedQuery

--- src/Vit.Linq/ComponentModel/PagedQuery.cs
+++ src/Vit.Linq/ComponentModel/PagedQuery.cs
@@ -7,9 +7,16 @@ namespace Vit.Linq.ComponentModel
     public class PagedQuery
     {
         public FilterRule filter { get; set; }
-        public IEnumerable<OrderField> orders { get; set; }
+        public List<OrderField> orders { get; set; }
         public PageInfo page { get; set; }
 
         public RangedQuery ToRangedQuery() => new RangedQuery { filter = filter, orders = orders, range = page?.ToRange() };
+
+        public static implicit operator RangedQuery(PagedQuery query) => query?.ToRangedQuery();
+
+    }
+    public class PagedQuery<T> : PagedQuery
+    {
+        public T extra { get; set; }
     }
 }
--- src/Vit.Linq/ComponentModel/RangedQuery.cs
+++ src/Vit.Linq/ComponentModel/RangedQuery.cs
@@ -7,7 +7,12 @@ namespace Vit.Linq.ComponentModel
     public class RangedQuery
     {
         public FilterRule filter { get; set; }
-        public IEnumerable<OrderField> orders { get; set; }
+        public List<OrderField> orders { get; set; }
         public RangeInfo range { get; set; }
+    }
+
+    public class RangedQuery<T> : RangedQuery
+    {
+        public T extra { get; set; }
     }
 }
